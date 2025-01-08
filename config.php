9940a3bc0ed5140fc01b12cec7cfe6a5668cd0e0
Skoyzz <e20243424@edu-st-jo.com>
2025-01-08 10:42:28 +0100

    v4.1.1.1

--- config.php
+++ config.php
@@ -2,4 +2,10 @@
 return [
     'smtp_password' => 'uD^5m@r6t}3*S9z',
 ];
+
+define('DB_HOST', 'host_bdd');
+define('DB_NAME', 'nom_de_la_bdd');
+define('DB_USER', 'utilisateur');
+define('DB_PASS', 'mot_de_passe');
+
 ?>
