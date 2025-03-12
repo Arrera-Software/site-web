e5a54fd34a4a936ac8a20046240dca4bb5f41bca
Baptiste P <baptiste.pauchet13@gmail.com>
2025-03-12 21:05:32 +0100

    Ajout footer

--- store.php
+++ store.php
@@ -106,4 +106,15 @@
             </div>
         </div>
     </main>
+
+
+     <!-- Footer -->
+     <footer class="main-footer">
+        <div class="container">
+            <p class="copyright">
+                © <?php echo date('Y'); ?> Arrera-Software | Tous droits réservés
+            </p>
+        </div>
+    </footer>
+
 </body>
