f2bda91fe1cf079c5c10ad9421d569a9287e31d5
Baptiste P <baptiste.pauchet13@gmail.com>
2026-05-03 22:00:38 +0200

    clear

--- hub.php
+++ hub.php
@@ -28,63 +28,7 @@
 
     ?>
 
-    <div class="container-g">
-        <div class="text-section">
-            <h1>Arrera Hub</h1>
-            <p></p>
-        </div>
-        <section class="image-section">
-            <img src="img/arrera-tiger.webp" alt="Arrera Interface I2024" class="img">
-        </section>
-        <a href="<?php echo getLink($pdo,'tiger-download'); ?>" class="download-interface">Télécharger l'installateur</a>
-    </div>
-
-    <div class="title-session">
-        <h1>Composant de l'Arrera Hub</h1>
-    </div>
-
-    <main class="services-container">
-
-        <div class="service-card">
-            <h2>GUI Arrera Tiger</h2>
-            <p>GUI Arrera Tiger est le nom de code du projet Arrera Store.</p>
-            <div class="service-links">
-                <a href="https://github.com/Arrera-Software/arrera-tiger" class="learn-more">Code source</a>
-            </div>
-        </div>
-
-        <div class="service-card">
-            <h2>Arrera Tiger Objet</h2>
-            <p>Arrera Tiger est le composant central d'Arrera Store, qui permet d'installer, de désinstaller et de mettre à jour vos applications Arrera.</p>
-            <div class="service-links">
-                <a href="https://github.com/Arrera-Software/Arrera-Tiger-objet" class="learn-more">Code source</a>
-            </div>
-        </div>
-
-        <div class="service-card">
-            <h2>Arrera Tiger Demon</h2>
-            <p>Arrera Tiger Demon est le composant présent dans tous les logiciels développés par Arrera, qui permet de vous indiquer si votre logiciel est à jour</p>
-            <div class="service-links">
-                <a href="https://github.com/Arrera-Software/tiger-demon" class="learn-more">Code source</a>
-            </div>
-        </div>
-
-        <div class="service-card">
-            <h2>Arrera Tiger Updater</h2>
-            <p>Arrera Tiger Updater est le programme qui permet d'effectuer l'installation d'Arrera Store</p>
-            <div class="service-links">
-                <a href="https://github.com/Arrera-Software/arrera-tiger-updater" class="learn-more">Code source</a>
-            </div>
-        </div>
-
-        <div class="service-card">
-            <h2>Fichier de depots</h2>
-            <p>Fichier JSON hébergé sur le site d'Arrera, qui permet aux deux autres composants de faire leur travail</p>
-            <div class="service-links">
-                <a href="https://arrera-software.fr/depots.json" class="learn-more">Fichier</a>
-            </div>
-        </div>
-    </main>
+    
 
 
     <?php include 'header-footer/footer.php'; ?>
