9d653337d87a09d259bd2792d468962699e7719b
Arrera Software <arrera-software@gmail.com>
2026-08-13 18:25:52 +0200

    Correction

--- assistant.php
+++ assistant.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assitant</title>
+    <title>Assistants</title>
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/assistant.css">
     <link rel="icon" href="img/logo-arrera.webp">
